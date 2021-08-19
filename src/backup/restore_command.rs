4b821d4c1b2351a7daf45f16708517f15b6e1b89
Xendergo <xendergo@protonmail.com>
2021-08-19 17:49:18 -0400

    Delete files before restoring

--- src/backup/restore_command.rs
+++ src/backup/restore_command.rs
@@ -4,6 +4,7 @@ use crate::Command;
 use anyhow::Error;
 use anyhow::Result;
 use clap::ArgMatches;
+use std::ffi::OsStr;
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::PathBuf;
@@ -47,23 +48,39 @@ impl Command<'_> for RestoreCommand {
         let backups_folder = BackupsFolder::get()?;
         let folder_to_restore_to = backups_folder.parent().unwrap();
 
+        println!("Deleting existing files");
+
+        for path in fs::read_dir(folder_to_restore_to)? {
+            let path_unwraped = path?.path();
+
+            if path_unwraped.file_name() == Some(OsStr::new(".backups")) {
+                continue;
+            }
+
+            if path_unwraped.is_dir() {
+                fs::remove_dir_all(path_unwraped)?;
+            } else {
+                fs::remove_file(path_unwraped)?;
+            }
+        }
+
         let file_names = backup
             .file_names()
             .map(|v| v.to_string())
             .collect::<Vec<String>>();
 
+        println!("Restoring backup");
+
         for file_name in file_names {
             let mut file = backup.get_file(&file_name)?;
 
-            let path = folder_to_restore_to.join(file_name);
+            let path = folder_to_restore_to.join(&file_name);
 
             if file.is_dir() {
-                if !path.exists() {
-                    fs::create_dir(path)?;
-                }
-
                 continue;
             }
+
+            fs::create_dir_all(path.parent().unwrap())?;
 
             let mut out = File::create(path)?;
 
